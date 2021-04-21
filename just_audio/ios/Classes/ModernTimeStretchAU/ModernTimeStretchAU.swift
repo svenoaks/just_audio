11a182271b074aa64f3184ff6c5937aadb05a1bb
Steve <svenoaks@gmail.com>
2021-04-21 13:20:51 -0400

    starting AU

---
+++ just_audio/ios/Classes/ModernTimeStretchAU/ModernTimeStretchAU.swift
@@ -0,0 +1,40 @@
+//
+//  ModernTimeStretchAU.swift
+//  just_audio
+//
+//  Created by Steve Myers on 4/21/21.
+//
+
+import Foundation
+import AudioToolbox
+import AVFoundation
+import CoreAudioKit
+
+
+public class ModernTimeStretchAU: AUAudioUnit {
+    lazy private var inputBusArray: AUAudioUnitBusArray = {
+        AUAudioUnitBusArray(audioUnit: self,
+                            busType: .input,
+                            busses: [kernelAdapter.inputBus])
+    }()
+
+    lazy private var outputBusArray: AUAudioUnitBusArray = {
+        AUAudioUnitBusArray(audioUnit: self,
+                            busType: .output,
+                            busses: [kernelAdapter.outputBus])
+    }()
+    
+    public override var inputBusses: AUAudioUnitBusArray {
+        return inputBusArray
+    }
+
+    public override var outputBusses: AUAudioUnitBusArray {
+        return outputBusArray
+    }
+    
+    public override var supportsUserPresets: Bool {
+        return false
+    }
+    
+
+}
